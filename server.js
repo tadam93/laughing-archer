1356a6fa03aa5f1fa3b5b7f075e6c56b1186c0a0
tadam93 <trevoradam@Trevors-MBP.home>
2015-06-09 21:37:12 -0500

    Create basic chat system

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile('index.html');
 });
 
-http.listen(80, '192.241.222.126',  function(){
-  console.log(__dirname);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+   
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
